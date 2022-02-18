250e063e7ba77f88609cc5730f01aeec9f1359e8
xiangyanjin <xiangyanjin@bytedance.com>
2022-02-19 00:15:21 +0800

    use vite
    Change-Id: I1bbe37c94207c50f7b864b7a3c50e21fbbc569a7

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
-<template>
-  <Disa />
-</template>
-
-<script>
-import Disa from "./components/Disa.vue";
-
-export default {
-  name: "App",
-  components: {
-    Disa,
-  },
-};
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style></style>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
